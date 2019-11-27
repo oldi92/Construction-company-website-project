473389a9c081eede45a9da0dfbd2dbba55535917
Oldi <oldi.prillo@gmail.com>
2019-11-27 02:42:11 +0000

    first site upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+function navSlide(){
+
+    document.getElementById('nav-links').classList.toggle('active');
+    navLinks = document.getElementById('nav-links').getElementsByTagName('li');    
+   
+}
+
+
+
+/*          navLinks.style.animation = 'navLinkFade 0.05s ease forwards         */
+        
+
+    
